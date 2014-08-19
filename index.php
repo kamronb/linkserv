9fb996509d9631d5a4da2b21a4fd2b73bbb0aab9
Kamron J. Bennett <kamron@cmit.com.jm>
2014-08-19 17:38:45 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE HTML>
+<!--
+Author: Kamron Bennett
+Email: kamron@cmit.com.jm
+Date: 21.02.14
+Website: www.cmit.com.jm
+-->
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Open Source Free Adservice for your Website and Backlinks</title>
+<meta name="description" content="Free Advertising and Backlink Generation for your website. Sign up and install a widget and get a free ad placement on thousands of websites on the internet" />
+<meta name="keywords" content="advertise, marketing, backlinking, adserver, free advertisement, web ads, placement, open source" />
+<meta name="author" content="Kamron J. Bennett" />
+<meta name="google-site-verification" content="lV91YkbXrHt4Qv3F7bO_Yzru6FyupLyE04zN5eAdDxk" />
+<meta name="robots" content="index,follow" />
+<meta name="CONTENT-LANGUAGE" content="EN" />
+<meta name="revisit-after" content="7 days">
+<meta name="copyright" content="2014, CMIT - Solutions" />
+<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+</head>
+<body>
+<header>
+	<div id="header_holder">
+		<h1>Linkserv Adservice</h1>
+		<h1 class="h1_style">Free Advertising and Linkbuilding for the Web</h1>
+	</div>
+</header>
+
+
+<div id="body_holder">
+	<div id="site_name">
+	<h1>Linkserv Adserver</h1>
+	<h2>Get high exposure without the high cost</h2>
+	<p>Linkserv Adserver service - the new revolutionary way to get your product, brand, 
+	website or service the exposure that is needed without the cost of the other ad
+	choices out there. Most important, it is simple and always Free!!!</p>
+	<p><a href="#">Find out more &raquo;&raquo;</a></p>
+	</div>
+
+	<div id="ad_box_holder">
+		<div id="create_ad_box">
+		<h1><a href="">Create Free Ad Links Now</a></h1>
+		<h2>start your ad creation now &raquo;</h2>
+		</div>
+
+
+			<div id="top-left">
+				<h5>Learn More About it</h5>
+				<p>What you need to know</p>
+			</div>
+
+
+			<div id="top-middle">
+				<h5>See it in Action</h5>
+				<p>See sites that use it</p>
+			</div>
+
+
+			<div id="top-right">
+				<h5>Installation</h5>
+				<p>Install Adserv on your site</p>
+			</div>
+
+
+			<div id="bottom-left">
+				<h5>Linkserv Blog</h5>
+				<p>Keep up with us</p>
+			</div>
+
+
+			<div id="bottom-middle">
+				<h5>Contribute</h5>
+				<p>How to Help</p>
+			</div>
+
+
+			<div id="bottom-right">
+				<h5>Donate</h5>
+				<p>Donate to Adserv</p>
+			</div>
+	</div>
+<div class="clearthis">
+</div>
+</div>
+
+<div id="bottom_holder">
+	
+
+		<div id="release_box">
+		<h4>Releases and News</h4>
+		<h5>This is the Releases Box</h5>
+		<p>Hello World this is another thing that is 
+		to be a blurb of the original link</p>
+
+		<h5>This is the Releases Box</h5>
+		<p>Hello World this is another thing that is 
+		to be a blurb of the original link</p>
+
+		<h5>This is the Releases Box</h5>
+		<p>Hello World this is another thing that is 
+		to be a blurb of the original link. More text 
+		will go under this so it can look a little 
+		better and be a good indicator of what it should
+		typically look lik on the site</p>
+
+		<h5>This is the Releases Box</h5>
+		<p>Hello World this is another thing that is 
+		to be a blurb of the original link</p>
+
+		<h5>This is the Releases Box</h5>
+		<p>Hello World this is another thing that is 
+		to be a blurb of the original link</p>
+		</div>
+
+		<div id="middle_box">
+		<h3>This is the Releases Box</h3>
+		<p></p>
+		</div>
+
+		<div id="right_box">
+		<h3>This is the Releases Box</h3>
+		<p></p>
+		</div>
+
+
+
+<div class="clearthis">
+</div>
+
+</div>
+
+<footer>
+	<ul>
+		<li>&copy; 2014 Linkserv Adservices</li>
+		<li><a href="http://www.cmit.com.jm">Affordable Web Design Services</a> by:<a href="http://www.cmit.com.jm">CMIT - Solutions</a></li>
+		<li><a href="#">Privacy Statement</a></li>
+		<li><a href="#">Terms of Use</a></li>
+	</ul>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
